310c88fbd18d6376a2260cfefd799f328c990904
Pietra-Santos <pietra0608@gmail.com>
2021-04-04 19:33:59 -0300

    Att Repository Usuario #6

--- src/main/java/com/ecommerce/pedritastore/repositories/UsuarioRepository.java
+++ src/main/java/com/ecommerce/pedritastore/repositories/UsuarioRepository.java
@@ -3,11 +3,12 @@ package com.ecommerce.pedritastore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ecommerce.pedritastore.model.Produto;
+import com.ecommerce.pedritastore.model.Usuario;
 
 @Repository
 public interface UsuarioRepository {
-	public interface StoreRepository extends JpaRepository<Long,Produto> {
+	
+	public interface StoreRepository extends JpaRepository<Long,Usuario> {
 		
 	}
 
